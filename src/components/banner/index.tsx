554da7836c2c5dc92ae9d924f8dbc71e0539e698
DanielL29 <danielnatsufire@gmail.com>
2022-12-24 20:02:11 -0300

    feat: introduction banner of landing page

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./Banner.module.css";
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <h1>MIREIA BELMONTE</h1>
+      <h2>
+        ENSEÑA <span>NATACIÓN</span>
+      </h2>
+    </div>
+  );
+}
