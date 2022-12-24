9c76fe723fa4e1d42968cccb6b385fb567410165
DanielL29 <danielnatsufire@gmail.com>
2022-12-24 19:21:45 -0300

    feat: desktop header dropdown navigation

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./Dropdown.module.css";
+import useUser from "../../hooks/useUser";
+
+interface IDropdownProps {
+  dropdown: boolean;
+  setDropdown: (toggle: boolean) => void;
+}
+
+export default function Dropdown({ dropdown, setDropdown }: IDropdownProps) {
+  const { logout } = useUser();
+
+  function finishSession() {
+    logout();
+    setDropdown(false);
+  }
+
+  return (
+    <ul className={dropdown ? styles.showOptions : styles.hideOptions}>
+      <li>PANEL DE CONTROL</li>
+      <li>TUS CURSOS</li>
+      <li>PERFIL</li>
+      <li>CUENTA</li>
+      <li className={styles.logout} onClick={finishSession}>
+        CERRAR SESÍON
+      </li>
+    </ul>
+  );
+}
