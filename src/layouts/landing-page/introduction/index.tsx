16975de4beace447abd8decb75d07fe10eff4865
DanielL29 <danielnatsufire@gmail.com>
2022-12-24 20:18:15 -0300

    feat: joining introduction areas to main layout

---
+++ src/layouts/landing-page/introduction/index.tsx
@@ -0,0 +1,18 @@
+import Banner from "../../../components/banner";
+
+import Workload from "./workload";
+import Slider from "./slider";
+import ExclusiveContent from "./exclusive-content";
+
+import styles from "./Introduction.module.css";
+
+export default function Introduction() {
+  return (
+    <div className={styles.introduction}>
+      <Banner />
+      <Workload />
+      <Slider />
+      <ExclusiveContent />
+    </div>
+  );
+}
