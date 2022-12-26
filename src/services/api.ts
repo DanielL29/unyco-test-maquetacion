e02954dcf69ec51adac9d64dc126dab5ec59fc90
DanielL29 <danielnatsufire@gmail.com>
2022-12-26 16:08:09 -0300

    feat: add new instances of country and whatsapp api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API_URL,
+export const unycosInstanceApi = axios.create({
+  baseURL: import.meta.env.VITE_BASE_UNYCOS_API_URL,
 });
 
-export default instance;
+export const restCountriesinstanceApi = axios.create({
+  baseURL: import.meta.env.VITE_BASE_REST_COUNTRIES_API_URL,
+});
+
+export const whatsappInstanceApi = import.meta.env.VITE_BASE_WHATSAPP_API_URL;
--- src/services/spotlights.ts
+++ src/services/spotlights.ts
@@ -1,12 +1,15 @@
-import api from "./api";
+import { unycosInstanceApi } from "./api";
 
 export async function getSpotlights() {
-  const response = await api.get("/u/courses/spotlights/natacion", {
-    headers: {
-      ["Content-Type"]: "application/json",
-      ["x-mejor-key"]: "unycos",
-    },
-  });
+  const response = await unycosInstanceApi.get(
+    "/u/courses/spotlights/natacion",
+    {
+      headers: {
+        ["Content-Type"]: "application/json",
+        ["x-mejor-key"]: "unycos",
+      },
+    }
+  );
 
   return response.data.spotlights;
 }
