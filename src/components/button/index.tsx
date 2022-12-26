78ec89a918559c5c5e5113432af4aac25e1c40c9
DanielL29 <danielnatsufire@gmail.com>
2022-12-25 21:17:13 -0300

    feat: add some button props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,18 +4,32 @@ interface IButtonProps {
   text: string;
   theme?: "yellow" | "transparent";
   hidden?: boolean;
+  height?: number;
+  width?: number;
+  marginTop?: number;
+  marginBottom?: number;
 }
 
 export default function Button({
   text,
   theme = "yellow",
   hidden = false,
+  height,
+  width,
+  marginTop,
+  marginBottom,
 }: IButtonProps) {
   return (
     <button
       className={`${styles.button} ${styles[theme]} ${
         hidden ? styles.hidden : ""
       }`}
+      style={{
+        height: `${height}px`,
+        width: `${width}px`,
+        marginTop: `${marginTop}px`,
+        marginBottom: `${marginBottom}px`,
+      }}
     >
       {text}
     </button>
