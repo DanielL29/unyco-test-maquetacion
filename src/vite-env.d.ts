41bc6b4486d61b639b8ef43d3e3966c5a6fa5a18
DanielL29 <danielnatsufire@gmail.com>
2022-12-26 16:06:39 -0300

    feat: add new env variables to countries and whatsapp api

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_BASE_API_URL: string;
+  readonly VITE_BASE_UNYCOS_API_URL: string;
+  readonly VITE_BASE_REST_COUNTRIES_API_URL: string;
+  readonly VITE_BASE_WHATSAPP_API_URL: string;
 }
 
 interface ImportMeta {
