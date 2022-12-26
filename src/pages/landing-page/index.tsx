41f56ef6b294e2397b9e536dcad66b0fc95eedce
DanielL29 <danielnatsufire@gmail.com>
2022-12-26 20:05:03 -0300

    feat: add relation articles to landing page

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -2,6 +2,7 @@ import Introduction from "../../layouts/landing-page/introduction";
 import LessonPlan from "../../layouts/landing-page/lesson-plan";
 import MoreCourses from "../../layouts/landing-page/more-courses";
 import MoreInformation from "../../layouts/landing-page/more-information";
+import RelationArticles from "../../layouts/landing-page/relation-articles";
 import StudentComments from "../../layouts/landing-page/student-comments";
 import styles from "./LandingPage.module.css";
 
@@ -22,6 +23,9 @@ export default function LandingPage() {
       </section>
       <section>
         <StudentComments />
+      </section>
+      <section>
+        <RelationArticles />
       </section>
     </main>
   );
